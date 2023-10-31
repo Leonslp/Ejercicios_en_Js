6f782feb68fa17376a67f0aa478f60e0600eb536
Santiago Leon <93327269+Leonslp@users.noreply.github.com>
2023-10-30 23:28:48 -0500

    Ejercicio 1.1, 1.2, 1.3.

--- codigo.js
+++ codigo.js
@@ -1,1 +1,4 @@
-console.log("Hola Mundo!");
+alert("Hola Mundo!");
+alert("Soy el primer script" )
+
+document.write("En el siguiente codigo se muestran dos mensajes con la etiqueta alert; esos mensajes se encuentran en un archivo Js que es llamado desde uno HTML con la etiqueta Script")
