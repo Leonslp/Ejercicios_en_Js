b60fee0429dc3f6b80667eb1030d2ea4a9f84d1a
Santiago Leon <93327269+Leonslp@users.noreply.github.com>
2023-10-31 20:57:46 -0500

    Segundo ejercicio Js

--- codigo.js
+++ codigo.js
@@ -1,4 +1,12 @@
-alert("Hola Mundo!");
-alert("Soy el primer script" )
+//En el siguiente codigo se muestran dos mensajes con la etiqueta alert; esos mensajes se encuentran en un archivo Js que es llamado desde uno HTML con la etiqueta Script
 
-document.write("En el siguiente codigo se muestran dos mensajes con la etiqueta alert; esos mensajes se encuentran en un archivo Js que es llamado desde uno HTML con la etiqueta Script")
+function mensajes() {
+    let holaMundo = "Hola Mundo!";
+    let primerScript = "Soy el primer script";
+    
+    return { holaMundo, primerScript };
+}
+
+let mensaje = mensajes();
+document.write(mensaje.holaMundo);
+document.write(mensaje.primerScript);
